0b2372a22f783a49cb5b9deb772424d2441b7223
Pixelsmith <144948098+marceloATpixelsmith@users.noreply.github.com>
2026-08-11 11:56:10 -0600

    Use secure Postgres URL at runtime

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -2,12 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 import dotenv from 'dotenv';
+import { getPostgresConnectionUrl } from './connection-url';
 
 dotenv.config();
 
-if (!process.env.POSTGRES_URL) {
-  throw new Error('POSTGRES_URL environment variable is not set');
-}
-
-export const client = postgres(process.env.POSTGRES_URL);
+export const client = postgres(getPostgresConnectionUrl());
 export const db = drizzle(client, { schema });
