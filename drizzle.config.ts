2eeeb91377a7cc00c93150447aa7520831cbbe9c
Pixelsmith <144948098+marceloATpixelsmith@users.noreply.github.com>
2026-08-11 11:20:39 -0600

    chore: isolate PostgreSQL objects in idoc schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,11 @@ export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
   dialect: 'postgresql',
+  schemaFilter: ['idoc'],
+  migrations: {
+    schema: 'idoc',
+    table: '__drizzle_migrations',
+  },
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
   },
