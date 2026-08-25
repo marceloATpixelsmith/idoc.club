d506da15b7e9e62d74d69e4083c923a7370b0d4f
Pixelsmith <144948098+marceloATpixelsmith@users.noreply.github.com>
2026-08-25 09:34:25 -0600

    Add onboarding consent persistence [skip ci]

---
+++ lib/db/migrations/0019_onboarding_consents.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "idoc"."onboarding_consents" (
+  "profile_id" integer PRIMARY KEY NOT NULL,
+  "terms_accepted_at" timestamp with time zone NOT NULL,
+  "privacy_accepted_at" timestamp with time zone NOT NULL,
+  "keep_updated_opt_in" boolean DEFAULT false NOT NULL,
+  "created_at" timestamp with time zone DEFAULT now() NOT NULL,
+  CONSTRAINT "onboarding_consents_profile_id_profiles_id_fk"
+    FOREIGN KEY ("profile_id") REFERENCES "idoc"."profiles"("id") ON DELETE cascade
+);
