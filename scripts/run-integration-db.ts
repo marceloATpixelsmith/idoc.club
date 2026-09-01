c5e068f1505ab40fea4e6f7743b2476f4727db47
Pixelsmith <144948098+marceloATpixelsmith@users.noreply.github.com>
2026-08-31 20:44:04 -0600

    Make server-action integration tests runnable under Node

--- scripts/run-integration-db.ts
+++ scripts/run-integration-db.ts
@@ -36,6 +36,8 @@ export async function runSuite(url: string, environment = process.env) {
   await run(process.execPath, [
     '--disable-warning=MODULE_TYPELESS_PACKAGE_JSON',
     '--import',
+    './tests/register-next-navigation-test-hook.mjs',
+    '--import',
     'tsx',
     '--conditions=react-server',
     '--test',
