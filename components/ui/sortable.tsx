9250fd7376fead6ff23f3e95c39e4f2d4944b2c7
Pixelsmith <144948098+marceloATpixelsmith@users.noreply.github.com>
2026-09-09 11:34:47 -0600

    Address table review findings

--- components/ui/sortable.tsx
+++ components/ui/sortable.tsx
@@ -484,7 +484,7 @@ function SortableItemHandle(props: SortableItemHandleProps) {
   const isDisabled = disabled ?? itemContext.disabled;
 
   const composedRef = useComposedRefs(ref, (node) => {
-    if (!isDisabled) return;
+    if (isDisabled) return;
     itemContext.setActivatorNodeRef(node);
   });
 
